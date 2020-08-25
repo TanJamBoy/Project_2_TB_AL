600238127ebd9817c0224b50371f6e9af6e97350
Dr Dougie <aaron.m.larsh@gmail.com>
2020-08-24 19:51:27 -0700

    fixed up routes and got data base set up.

--- controllers/game_controller.js
+++ controllers/game_controller.js
@@ -1,13 +1,12 @@
 const controller = (app) => {
-    //html routes
     // Requiring path to so we can use relative routes to our HTML files
     const path = require("path");
     // Requiring our custom middleware for checking if a user is logged in
     const isAuthenticated = require("../config/middleware/isAuthenticated");
+    const db = require("../models/");
     const passport = require("../config/passport");
 
-    const db = require("../models/");
-
+    //html routes
     app.get("/", (req, res) => {
         // If the user already has an account send them to the members page
         if (req.user) {
@@ -23,7 +22,10 @@ const controller = (app) => {
         }
         res.render("login")
     });
-
+    app.get("/game", (req, res) => {
+        // If the user already has an account send them to the members page
+        res.render("game")
+    });
     // Here we've add our isAuthenticated middleware to this route.
     // If a user who is not logged in tries to access this route they will be redirected to the signup page
     app.get("/members", isAuthenticated, (req, res) => {
