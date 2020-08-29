f36e7051e6cc9626f563cfa7a22acd1729c940d3
Tanner Boyer <tannerjboyer@gmail.com>
2020-08-29 11:59:56 -0700

    more testing

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static("public/assets"));
 
 // We need to use sessions to keep track of our user's login status
 app.use(
-  session({ secret: "keyboard cat", resave: false, saveUninitialized: true })
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: false })
 );
 app.use(passport.initialize());
 app.use(passport.session());
